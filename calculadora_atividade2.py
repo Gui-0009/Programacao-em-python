ba0cdf4a8734cc475281d29d1a0b164d15293e89
Gui-0009 <kiberj2354@gmail.com>
2026-01-31 14:35:08 -0300

    Add files via upload

---
+++ calculadora_atividade2.py
@@ -0,0 +1,11 @@
+n1 = float( input('Digite um número') )
+n2 = float( input('Digite um número') )
+
+soma  =  n1 + n2
+subtracao = n1 - n2
+multiplicacao = n1 * n2
+divisao = n1 / n2
+print('Resuldado da soma é:', soma)
+print('Resuldado da subtração é:', subtracao)
+print('Resuldado da multiplicação é:', multiplicacao)
+print('Resuldado da divisão é:', divisao)
