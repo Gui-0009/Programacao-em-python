0dc47fd7190912800752ea4ecf6976c1699e8005
Gui-0009 <kiberj2354@gmail.com>
2026-01-31 17:27:16 -0300

    Add files via upload

---
+++ aula_5/index.py
@@ -0,0 +1,69 @@
+#1 Imprima uma mensagem de boas-vindas na tela.
+p1 = 'hello '
+p2 = 'World'
+print(p1 + p2)
+
+#2 Declare uma variável booleana verdadeiro com o valor True e imprima seu tipo
+
+lista_espera = True
+foi_chamado = False
+print("Está esperando?", lista_espera, 'Já foi chamado?', foi_chamado)
+
+#3 Imprima o resultado da multiplicação de dois números decimais de sua escolha
+
+n1 = 40
+operacao = '*'
+n2 = 40
+print(n1, operacao, n2,"=", n1 * n2)
+
+#4 Imprima o resultado da divisão (/)de dois números inteiros de sua escolha.
+
+n1 = 22
+operacao = '/'
+n2 = 33
+print (n1, operacao, n2,"=", n1 / n2)
+
+#5 Imprima o resultado da subtração de dois números inteiros de sua escolha
+
+n1 = 67
+operacao = '-'
+n2 = 55
+print (n1, operacao, n2,"=", n1 - n2)
+
+#6 Imprima o resultado da divisão (//)inteira de dois números inteiros de sua escolha.
+
+n1 = 22
+operacao = '/'
+n2 = 33
+print (n1, operacao, n2, "=", n1 // n2)
+
+#7 Imprima o resultado da multiplicação de 4 números decimais de sua escolha
+
+n1 = 40
+operacao = '*'
+n2 = 40
+n3 = 100
+n4= 0
+print(n1, operacao, n2, operacao, n3, operacao, n4,"=",  n1 * n2 * n3 * n4)
+
+#8 Declare uma variável numero e atribua um número inteiro. Em seguida, imprima o dobro desse número
+
+numero = 99
+print(numero * 2)
+
+#9 Crie um sistema de cadastro com as estruturas que vc já conhece(Use apenas input, print e variavel)
+# nome, email, idade, cidade, gradução, estado civil 
+
+nome = input('Digite seu nome: ')
+print()
+email = input('Digite seu E-mail: ')
+print()
+idade = input('Digite sua idade: ')
+print()
+cidade = input('Digit o nome da sua cidade: ')
+print()
+graduacao = input('Digite sua Graduação: ')
+print()
+estado_civil = input('Digite o seu estado civil: ')
+print()
+print('Nome:', nome,';', 'Idade:', idade,';', 'E-mail:', email,';', 'Cidade:', cidade,';', 'Graduação:', graduacao,';', "Estado civil:", estado_civil, '.')
